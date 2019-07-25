0aa59dabe9f25f6fe84767e616c5f7794a4a9390
Tom Roche <tomrochee@gmail.com>
2019-07-25 22:35:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+window.onload = choosePic;
+
+var myPix = new Array(
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg"
+);
+var myPix2 = new Array(
+  "img2/1.jpg",
+  "img2/2.jpg",
+  "img2/3.jpg",
+  "img2/4.jpg",
+  "img2/5.jpg",
+  "img2/6.jpg",
+  "img2/7.jpg",
+  "img2/8.jpg",
+  "img2/10.jpg"
+);
+function choosePic() {
+  var randomNum = Math.floor(Math.random() * myPix2.length);
+  document.getElementById("myPicture2").src = myPix2[randomNum];
+
+  var randomNum = Math.floor(Math.random() * myPix.length);
+  document.getElementById("myPicture").src = myPix[randomNum];
+}
+/*/
+//2nd img
+
+window.onload = choosePic;
+
+var myPix = new Array(
+    "img/1.jpg",
+    "img/2.jpg",
+    "img/3.jpg",
+    "img/4.jpg",
+    "img/5.jpg"
+)
+
+
+function choosePic2() {
+  var randomNum = Math.floor(Math.random() * myPix2.length);
+  document.getElementById("myPicture2").src = myPix2[randomNum];
+}
+/*/
